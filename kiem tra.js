08a7bc79122e1cb3200aef2cf896426681f91f56
chocon123 <77884754+chocon123@users.noreply.github.com>
2021-03-27 15:33:29 +0700

    Add files via upload

---
+++ kiem tra.js
@@ -0,0 +1,39 @@
+// 1 D 
+// 2 B
+// 3 C 
+// 4 A
+ 
+let a = setInterval(function(){
+    console.log(`len luon`);
+},1000)
+
+
+
+
+
+
+
+
+let li = document.getElementsByTagName(`li`);
+console.log(li[1]);
+for(let i of li){
+    console.log(i);
+}
+
+let div = document.getElementsByClassName(`singer`)
+console.log(div[1]);
+for (let o of div){
+    console.log(o);
+}
+
+singer[1].remove
+
+
+function wish(){
+    let name = `Thuy`;
+    let wish =`bo doi`;
+    alert(`name: ${name} you have the: ${wish}`)
+}
+wish();
+
+
